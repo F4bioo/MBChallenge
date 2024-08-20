144b847e26fd56b0d8b87b6daf6faf89b8fd84f1
F4bioo <costa.fbo@gmail.com>
2024-08-20 16:08:31 -0300

    Added: Setup common viewmodel

---
+++ libraries/arch/src/main/java/com/fappslab/mbchallenge/libraries/arch/viewmodel/extension/ViewExtension.kt
@@ -0,0 +1,28 @@
+package com.fappslab.mbchallenge.libraries.arch.viewmodel.extension
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+@Composable
+inline fun <reified T> Flow<T>.observeAsEvents(
+    crossinline eventBlock: suspend (T) -> Unit
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(key1 = this, lifecycleOwner.lifecycle) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            this@observeAsEvents.flowWithLifecycle(
+                lifecycleOwner.lifecycle,
+                Lifecycle.State.STARTED
+            ).collect { eventBlock(it) }
+        }
+    }
+}
